60fa69e9004150c4561e421540c2f39a29abd0de
Dushan Terzikj <dusanterzic87@gmail.com>
2017-03-14 13:06:08 +0100

    Use OnClickListener in MainActivity.java insted of onClick label in activity_main.xml

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -21,37 +21,69 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // find TextViews by their IDs
+        TextView numbersTextView = (TextView) findViewById(R.id.numbers);
+        TextView phrasesTextView = (TextView) findViewById(R.id.phrases);
+        TextView familyTextView = (TextView) findViewById(R.id.family);
+        TextView colorsTextView = (TextView) findViewById(R.id.colors);
+
+        // set click listeners
+        numbersTextView.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View view) {
+
+                Intent newActivity = new Intent(MainActivity.this, NumbersActivity.class);
+                Toast.makeText(MainActivity.this, "Open " + getResources().getString(R.string.category_numbers)
+                        + " category", Toast.LENGTH_SHORT).show();
+                startActivity(newActivity);
+            }
+        });
+
+        phrasesTextView.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+
+                Intent newActivity = new Intent(MainActivity.this, PhrasesActivity.class);
+                Toast.makeText(MainActivity.this, "Open " + getResources().getString(R.string.category_phrases)
+                        + " category", Toast.LENGTH_SHORT).show();
+                startActivity(newActivity);
+            }
+        });
+
+        familyTextView.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+
+                Intent newActivity = new Intent(MainActivity.this, FamilyActivity.class);
+                Toast.makeText(MainActivity.this, "Open " + getResources().getString(R.string.category_family)
+                        + " category", Toast.LENGTH_SHORT).show();
+                startActivity(newActivity);
+            }
+        });
+
+        colorsTextView.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+
+                Intent newActivity = new Intent(MainActivity.this, ColorsActivity.class);
+                Toast.makeText(MainActivity.this, "Open " + getResources().getString(R.string.category_colors)
+                        + " category", Toast.LENGTH_SHORT).show();
+                startActivity(newActivity);
+            }
+        });
     }
 
-    public void openNumbersCategory(View view){
 
-        Intent intent = new Intent(this, NumbersActivity.class);
-        startActivity(intent);
-    }
-
-    public void openColorCategory(View view){
-
-        Intent intent = new Intent(this, ColorsActivity.class);
-        startActivity(intent);
-    }
-
-    public void openFamilyCategory(View view){
-
-        Intent intent = new Intent(this, FamilyActivity.class);
-        startActivity(intent);
-    }
-
-    public void openPhrasesCategory(View view){
-
-        Intent intent = new Intent(this, PhrasesActivity.class);
-        startActivity(intent);
-    }
 }
