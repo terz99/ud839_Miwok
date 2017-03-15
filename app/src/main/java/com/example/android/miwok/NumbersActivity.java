3bf9bcfaf6e7d04d45eae8958020e1b7e683551f
Dushan Terzikj <dusanterzic87@gmail.com>
2017-03-15 01:52:50 +0100

    Set ArrayAdapter and ListView in NumbersActivity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,12 +2,49 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // add numbers
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+        words.add("eleven");
+        words.add("twelve");
+        words.add("thirteen");
+        words.add("fourteen");
+        words.add("fifteen");
+        words.add("sixteen");
+        words.add("seventeen");
+        words.add("eighteen");
+        words.add("nineteen");
+        words.add("twenty");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+        ListView numbersListView = (ListView) findViewById(R.id.numbers_list_view);
+        numbersListView.setAdapter(adapter);
     }
 }
